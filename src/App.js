18f1b0d6364ac6f3ed5e360345fea02e2d4a15d9
Jeff <li.jing@smartcompany.jp>
2022-04-23 09:20:11 +0900

    add function

--- src/App.js
+++ src/App.js
@@ -25,11 +25,23 @@ function App() {
   //input 框在失去焦点后编辑弹窗消失,弹窗出现之后 input 能自动获取焦点的效果
   //但是需要注意的是，input 元素是在 show 变成 true ，并且组件真实 DOM 再次渲染完成之后才能获取引用
   useEffect(() => {
-    // show 变成 true，并且组件渲染完成之后执行
+    // 因为show 变成 true，并且是在组件渲染完成之后执行,所以写在useEffect里面
     if (show) {
       inputRef.current?.focus();
     }
   }, [show]);
+
+  function add() {
+    jobs.push({
+      id: randomId(),
+      desc,
+      isSelected: false,
+      createTime: Date.now(),
+    });
+    setJobs([...jobs]);
+    setDesc("");
+  }
+
   return (
     <div className="App">
       {show ? (
